80ae281aded7db216acb6f2fdc9dd239426b85f7
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-25 20:08:38 +0530

    youtube

---
+++ YouTube/youtube/src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
